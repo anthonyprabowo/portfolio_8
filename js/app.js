131124466e54aa8bb733bcb14100de1cc2338d19
Anthony Prabowo <anthonyprabowo@users.noreply.github.com>
2020-12-18 18:30:20 -0800

    Adding next and previous funcationality and grid view for different viewport

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 // ---------------------
 // VARIABLES
 // ---------------------
-const employees = document.getElementById('employee')
+const employees = document.getElementById('employee-container')
 const wait = document.getElementById('wait-container')
+const employeesArray = [];
+const employeesDetailArray = [];
+const modalClose = document.querySelector(".close");
+const cards = document.getElementsByClassName("card");
+const modalContainer = document.getElementById("modal-container");
+const modalContent = document.querySelector('.modal-content').children;
+const modal = document.querySelector('.modal-content');
+const nextArrow = document.getElementById('next-arrow');
+const backArrow = document.getElementById('back-arrow');
+let modalClicked = 0;
+
+
 
 // ---------------------
 //  FETCH FUNCTIONS
 // ---------------------
-    fetchData('https://randomuser.me/api/?results=12');
+
+fetchData('https://randomuser.me/api/?results=12');
 
 
 
@@ -15,29 +28,136 @@ const wait = document.getElementById('wait-container')
 //  HELPER FUNCTIONS
 // ---------------------
 
+function checkStatus(response) {
+    if(response.ok === true) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+
+
 async function fetchData(url) {
     let i = 0;
     wait.innerHTML="LOADING..."
     await fetch(url)
+        .then(response => checkStatus(response))
         .then(data => data.json()) // parse the data to json
         .then(obj => obj.results)
-        .then(result => console.log(result))
+        .then(result => {
+            for(let i = 0; i < 12; i++) {
+                generateHTML(result[i])
+            }
+            displayHTML();
+            employeesDetailArray.push(result);
+        })
         // .then(result => result[0]);
-        .catch(err => console.log(err));
-    wait.style.display="none";
+        .catch(err => {
+            wait.innerHTML = "Something Went Wrong! :(";
+            console.log(err);
+        });
+    await modalEventListener();
+    if(wait.innerHTML === "LOADING..."){
+        wait.style.display="none";
+    }
 }
 
 function generateHTML(obj) {
     const html = `
-            <div class=card>
-                <img src=${obj.picture.large} alt="">
-                <div class="employee-details">
-                    <h2>${obj.name.first} ${employee.name.last}</h2>
-                    <p>${obj.email}</p>
-                    <p>${obj.location.city}
+            <div class="card">
+                <div id="employee">
+                    <img src=${obj.picture.large} alt="">
+                    <div class="employee-details">
+                        <h2>${obj.name.first} ${obj.name.last}</h2>
+                        <p>${obj.email}</p>
+                        <p>${obj.location.city}
+                    </div>
                 </div>
             </div>
         `
-        employees.innerHTML = html;
+    employeesArray.push(html);
 }
+
+function displayHTML() {
+    let htmlBuilder = ''
+    for(let i = 0; i < employeesArray.length; i++) {
+        htmlBuilder += employeesArray[i];
+    }
+    employees.innerHTML = htmlBuilder;
+}
+
+function createModal(num) {
+    const employeesDetail = employeesDetailArray[0]
+    birthday = employeesDetail[num].dob.date.toString();
+    modalContainer.style.display = 'block';
+    modalContent[1].src = employeesDetail[num].picture.large;
+    modalContent[2].innerHTML = `${employeesDetail[num].name.first} ${employeesDetail[num].name.last}`;
+    modalContent[3].innerHTML = `${employeesDetail[num].email}`;
+    modalContent[4].innerHTML = `${employeesDetail[num].location.city}`;
+    modalContent[6].innerHTML = `${employeesDetail[num].phone}`;
+    modalContent[7].innerHTML = `${employeesDetail[num].location.street.number} ${employeesDetail[num].location.street.name}, ${employeesDetail[num].location.state} ${employeesDetail[num].location.postcode}`
+    modalContent[8].innerHTML = `Birthday: ${birthday.substring(5,7)}/${birthday.substring(8,10)}/${birthday.substring(0,4)}`;
+}
+
+// ---------------------
+//  EVENT LISTENER
+// ---------------------
+
+function modalEventListener() {
+    const employeesDetail = employeesDetailArray[0]
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', () => {
+            modal.style.animation = 'pull-down .3s ease-in-out forwards';
+            createModal(i);
+            modalClicked = i;
+        })
+    }
+}
+
+// When the user clicks the x button
+modalClose.addEventListener('click', () => {
+    modal.style.animation = 'pull-up .3s ease-in-out forwards';
+    setTimeout(() => modalContainer.style.display = 'none', 300);
+
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalContainer) {
+        modal.style.animation = 'pull-up .3s ease-in-out forwards';
+        setTimeout(() => modalContainer.style.display = 'none', 300);
+    }
+}
+
+// When the user clicks the next button on modal
+nextArrow.addEventListener('click', () => {
+    setTimeout(() => {
+        // if the last modal is clicked, clicking the next arrow will return to number 0
+        if(modalClicked === 11){
+            modalClicked = 0;
+        } else { // else, increase the modal number by 1
+            modalClicked += 1;
+        }
+        createModal(modalClicked);
+    }, 500);
+    modal.style.animation = 'slide-right 1s ease-in-out forwards';
+    setTimeout(() => modal.style.animation = 'none', 1000);
+})
+
+backArrow.addEventListener('click', () => {
+    setTimeout(() => {
+        if(modalClicked === 0) {
+            modalClicked = 11;
+        } else {
+            modalClicked -= 1;
+        }
+        createModal(modalClicked);
+    }, 500);
+    modal.style.animation = 'slide-left 1s ease-in-out forwards';
+    setTimeout(() => modal.style.animation = 'none', 1000);
+})
+
+
+
+
 
